84f2aafc3e6582d0b61d7cf858c83bbaca19ab1e
higashi1012 <hi9ga8shi7@gmail.com>
2023-01-03 18:55:55 +0900

    ITP1_3_Aを回答しました

---
+++ Lesson - ITP1/ITP1_3_A.py
@@ -0,0 +1,15 @@
+def main():
+    '''
+    cnt = 0
+    while cnt < 1000:
+        print('Hello World')
+        cnt += 1
+    '''
+    '''
+    for _ in range(1000):
+        print('Hello World')
+    '''
+    print('Hello World\n'*1000, end='')
+
+if __name__ == '__main__':
+    main()
