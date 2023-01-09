2f37b87bb4fcd2be42119c65f7a8c3bbe74ac9a2
higashi1012 <hi9ga8shi7@gmail.com>
2023-01-09 12:27:47 +0900

    ITP1_5_Cを回答しました。

---
+++ Lesson - ITP1/ITP1_5_C.py
@@ -0,0 +1,16 @@
+def main():
+    while True:
+        h, w = map(int, input().split())
+        if h == w == 0:
+            break
+        for i in range(h):
+            for j in range(w):
+                if (i+j)%2 == 0:
+                    print('#', end='')
+                else:
+                    print('.', end='')
+            print()        
+        print()          
+
+if __name__ == '__main__':
+    main()
